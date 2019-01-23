6b5af191b9148fe34574c5d13d4dd6fc2ac880c2
Kwadwo Agyapon-Ntra <kwadwo.agyapon-ntra@meltwater.org>
2019-01-23 10:27:18 +0000

    Fetching robot users from online REST API using fetch()

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
 import SearchBox from './SearchBox';
 import './App.css';
 
@@ -8,9 +7,17 @@ class App extends Component {
     constructor(){
         super();
         this.state = {
-            robots,
+            robots: [],
             searchField: ''
         }
+    }
+    
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(users => {
+            this.setState({robots: users})
+        });
     }
 
     // to avoid problems with 'this' in your class fnxs, use arrow fxns
@@ -21,14 +28,17 @@ class App extends Component {
     render(){
         const filteredRobots = this.state.robots.filter(robot =>{
             return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
-        }); 
-        return (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots} />
-            </div>
-        );
+        });
+        if(this.state.robots.length === 0)
+            return <h1 className="tc">Loading...</h1>
+        else
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots={filteredRobots} />
+                </div>
+            );
     }
 };
 
