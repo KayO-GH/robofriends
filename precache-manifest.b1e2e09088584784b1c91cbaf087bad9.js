a431484811aede8113df6b8a8d1fb1b955c6d25d
Kwadwo Agyapon-Ntra <kwadwo.agyapon-ntra@meltwater.org>
2019-09-24 18:14:32 +0000

    Updates

---
+++ precache-manifest.b1e2e09088584784b1c91cbaf087bad9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "9e9b2397f27dae6f3968",
+    "url": "/robofriends/static/js/main.9e9b2397.chunk.js"
+  },
+  {
+    "revision": "6b0e820dd914825c2cfe",
+    "url": "/robofriends/static/js/1.6b0e820d.chunk.js"
+  },
+  {
+    "revision": "9e9b2397f27dae6f3968",
+    "url": "/robofriends/static/css/main.fe9577b7.chunk.css"
+  },
+  {
+    "revision": "6b0e820dd914825c2cfe",
+    "url": "/robofriends/static/css/1.b785f241.chunk.css"
+  },
+  {
+    "revision": "b3e4089b60ff648c4f5c3950fd2c9c8a",
+    "url": "/robofriends/index.html"
+  }
+];
