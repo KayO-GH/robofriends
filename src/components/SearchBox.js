5cc27682af1cdcaa42011aaeb0d4d942461e23d4
Kwadwo Agyapon-Ntra <kwadwo.agyapon-ntra@meltwater.org>
2019-09-25 10:44:18 +0000

    Improved pe audirit concerns

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const SearchBox = ({ searchChange }) => {
-    return (
-        <div className='pa2'>
-            <input
-                className='pa3 ba b--green bg-lightest-blue'
-                type='search'
-                placeholder='Search Robots'
-                onChange={searchChange}//onchange, call the searchChange fxn, which was passed in as a prop
-            />
-        </div>
-    );
+  return (
+    <div className="pa2">
+      <input
+        aria-label="Search Robots"
+        className="pa3 ba b--green bg-lightest-blue"
+        type="search"
+        placeholder="Search Robots"
+        onChange={searchChange} //onchange, call the searchChange fxn, which was passed in as a prop
+      />
+    </div>
+  );
 };
 
 export default SearchBox;
