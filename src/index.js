a4312fa56b71b7059dd09d21b052f9a3d50ba684
Kwadwo Agyapon-Ntra <kwadwo.agyapon-ntra@meltwater.org>
2020-06-12 11:08:00 +0000

    Brought logger back

--- src/index.js
+++ src/index.js
@@ -1,27 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { createLogger } from 'redux-logger';
-import thunkMiddleware from 'redux-thunk';//listens for and acts on functions that run in our actions
-import './index.css';
-import App from './containers/App';
-import * as serviceWorker from './serviceWorker';
-import 'tachyons';
-import { searchRobotsReducer, fetchRobotsReducer } from './reducers';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk"; //listens for and acts on functions that run in our actions
+import "./index.css";
+import App from "./containers/App";
+import * as serviceWorker from "./serviceWorker";
+import "tachyons";
+import { searchRobotsReducer, fetchRobotsReducer } from "./reducers";
 
 const logger = createLogger();
 
-const rootReducer = combineReducers({searchRobotsReducer, fetchRobotsReducer});
+const rootReducer = combineReducers({
+  searchRobotsReducer,
+  fetchRobotsReducer,
+});
 
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware, logger)
+);
 
 ReactDOM.render(
-    //Provider component wraps the app, allowing all child components to access the store using connect()
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
+  //Provider component wraps the app, allowing all child components to access the store using connect()
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
